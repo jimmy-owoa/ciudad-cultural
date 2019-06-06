08c2209476407df2e32ffd01073073e35ae106b0
ALONPC <alon.berrios@gmail.com>
2019-06-06 19:18:33 -0400

    Member model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
+    resources :members
+  end
+  namespace :admin do
     resources :events
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
